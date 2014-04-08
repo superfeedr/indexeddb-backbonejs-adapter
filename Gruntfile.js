8405df5ecdd6dd4a5f9205c9ac2bcd2f0a0fc926
andornaut <andornaut@gmail.com>
2014-04-07 22:39:25 -0400

    Convert spaces to tabs in Gruntfile.js.

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,24 +11,24 @@ module.exports = function(grunt) {
 
 		'saucelabs-qunit': {
 			all: {
-                options: {
-                    username: 'idbbackbone',
-                    key: '6ffa8cb3-47f3-4532-aedb-aee964d0eefb',
-                    urls: ['http://127.0.0.1:9999/tests/test.html'],
-                    build: process.env.TRAVIS_JOB_ID,
-                    concurrency: 2,
-                    testname: 'qunit tests',
-                    browsers: [{
-                        browserName: 'chrome',
-                        platform: 'linux'
-                    }]
-                }
-            }
+				options: {
+		    			username: 'idbbackbone',
+		    			key: '6ffa8cb3-47f3-4532-aedb-aee964d0eefb',
+		    			urls: ['http://127.0.0.1:9999/tests/test.html'],
+		    			build: process.env.TRAVIS_JOB_ID,
+		    			concurrency: 2,
+		    			testname: 'qunit tests',
+		    			browsers: [{
+						browserName: 'chrome',
+						platform: 'linux'
+		    			}]
+				}
+	    		}
 		},
 		watch: {}
 	});
 
-    // Loading dependencies
+    	// Load dependencies
 	for (var key in grunt.file.readJSON('package.json').devDependencies) {
 		if (key !== 'grunt' && key.indexOf('grunt') === 0) grunt.loadNpmTasks(key);
 	}
